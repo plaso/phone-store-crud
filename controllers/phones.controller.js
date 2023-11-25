b2db85bdaa46329724cb98ab15b65e8e1cc31fdb
Pablo Laso Cortabitarte <plasocortabitarte@gmail.com>
2023-11-25 12:35:19 +0100

    edit and delete

--- controllers/phones.controller.js
+++ controllers/phones.controller.js
@@ -37,7 +37,7 @@ module.exports.getPhoneCreateForm = (req, res, next) => {
 module.exports.doPhoneCreate = (req, res, next) => {
   Phone.create(req.body)
     .then(phoneDB => {
-      res.redirect('/smartphones');
+      res.redirect(`/smartphones/${phoneDB._id}`);
     })
     .catch(err => {
       if (err instanceof mongoose.Error.ValidationError) {
@@ -47,4 +47,45 @@ module.exports.doPhoneCreate = (req, res, next) => {
         next(err)
       }
     })
+}
+
+module.exports.getPhoneEditForm = (req, res, next) => {
+  // Me traigo el phone a editar de la base de datos
+  Phone.findById(req.params.id)
+    .then(phone => {
+      if (phone) {
+        res.render('phones/form', { phone, isEdit: true })
+      } else {
+        next(createError(404, 'No hemos encontrado este smartphone'))
+      }
+    })
+    .catch(err => next(err))
+
+}
+
+module.exports.doPhoneEdit = (req, res, next) => {
+  Phone.findByIdAndUpdate(req.params.id, req.body, { new: true }) // Primer parametro id - segundo objeto de updates - tercero el new true si quereis el objeto actualizado
+    .then(phoneDB => {
+      res.redirect(`/smartphones/${phoneDB._id}`);
+    })
+    .catch(err => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        // Renderizar la vista de nuevo, pero con los errores
+        res.render('phones/form', { errors: err.errors, phone: req.body, isEdit: true })
+      } else {
+        next(err)
+      }
+    })
+}
+
+module.exports.deletePhone = (req, res, next) => {
+  Phone.findByIdAndDelete(req.params.id)
+    .then(phoneDB => {
+      if (phoneDB) {
+        res.redirect('/smartphones');
+      } else {
+        next(createError(404, 'No hemos encontrado este smartphone'))
+      }
+    })
+    .catch(err => next(err))
 }
--- routes/main.routes.js
+++ routes/main.routes.js
@@ -11,7 +11,16 @@ router.get('/smartphones', phonesController.getPhones);
 router.get('/smartphones/create', phonesController.getPhoneCreateForm); // Esta que vaya antes de la del id porque sino entraria en la otra
 router.post('/smartphones', phonesController.doPhoneCreate);
 
+// Update
+
+router.get('/smartphones/:id/edit', phonesController.getPhoneEditForm);
+router.post('/smartphones/:id', phonesController.doPhoneEdit);
+
 // Read detail
 router.get('/smartphones/:id', phonesController.getPhoneDetail);
+
+// Delete
+
+router.post('/smartphones/:id/delete', phonesController.deletePhone);
 
 module.exports = router;
